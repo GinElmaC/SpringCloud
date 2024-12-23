2014a27eeb16969766045fa9c84b5d5f55c37e96
GinElmaC <2227172350@qq.com>
2024-12-23 21:08:19 +0800

    2024.12.23 springcloud 今日学习：CircuitBreaker Resilience4J的服务熔断两种方式

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/Config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/Config/FeignConfig.java
@@ -7,12 +7,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+    /**
+     * 配置重试机制
+     * @return
+     */
     @Bean
     public Retryer myRetryer(){
-//        return Retryer.NEVER_RETRY;//Feign默认配置是不走重试策略的
-        return new Retryer.Default(100,1,3);
+        return Retryer.NEVER_RETRY;//Feign默认配置是不走重试策略的
+//        return new Retryer.Default(100,1,3);
     }
 
+    /**
+     * 配置我们的日志
+     * @return
+     */
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
